9f8612efda84a100307cda0b60a9953c215535b3
vikasfreelancercoding@gmail.com <vikasfreelancercoding@gmail.com>
2026-03-01 17:26:31 +0530

    newsletterbox added

---
+++ frontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+  return (
+    <div className='text-center'>
+      <p className='text-2xl font-medium text-gray-800'>Subcribe now & get 20% off </p>
+    </div>
+  )
+}
+
+export default NewsletterBox
